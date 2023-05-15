7ecb3410303fa0c39d7c90bcbe9b7735e99bf332
Karoline de Souza Quetz <kquetz@viceri.com.br>
2023-05-15 09:53:26 -0300

    feat: created design system

---
+++ packages/tokens/src/colors.ts
@@ -0,0 +1,14 @@
+export const colors = {
+  white: '#FFFFFF',
+  black: '#000000',
+
+  gray100: '#F9F9F9',
+
+  yellow: '#FED041',
+
+  purple: '#5C259D',
+
+  blue900: '#253267',
+
+  red: '#FE4239',
+}
---
+++ packages/tokens/src/index.ts
@@ -0,0 +1,7 @@
+export * from './colors'
+
+export * from './font-sizes'
+
+export * from './font-weights'
+
+export * from './fonts'

d2c076f26f4cc3219aa94b12975a4b618183a7ba
Karoline de Souza Quetz <kquetz@viceri.com.br>
2023-05-17 10:51:00 -0300

    feat: created eslint global and react configuration files

---
+++ packages/eslint-config/globals.js
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: [
+    'standard',
+    'plugin:prettier/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  rules: {
+    'prettier/prettier': ["error", {
+      'printWidth': 80,
+      'tabWidth': 2,
+      'singleQuote': false,
+      'trailingComma': 'all',
+      'arrowParens': 'always',
+      'semi': true,
+      'endOfLine': 'auto',
+    }],
+  },
+}

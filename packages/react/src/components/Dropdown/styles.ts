d512378c3fcedcea740bd66e9cc53e4d1bce8b34
isabelamacedo <isabelacb.macedo@gmail.com>
2023-05-23 14:28:00 -0300

    fix: styles

--- packages/react/src/components/Dropdown/styles.ts
+++ packages/react/src/components/Dropdown/styles.ts
@@ -7,5 +7,5 @@ export const dropdownContent = cva(
   'min-w-[220px] bg-white dark:bg-zinc-900 rounded-md p-[5px] shadow-[0px_10px_38px_-10px_rgba(22,_23,_24,_0.35),_0px_10px_20px_-15px_rgba(22,_23,_24,_0.2)] will-change-[opacity,transform] data-[side=top]:animate-slideDownAndFade data-[side=right]:animate-slideLeftAndFade data-[side=bottom]:animate-slideUpAndFade data-[side=left]:animate-slideRightAndFade',
 );
 export const dropdownItem = cva(
-  'group text-[13px] leading-none text-white font-semibold rounded-[3px] flex items-center h-[25px] px-[5px] relative pl-[25px] select-none outline-none data-[disabled]:text-mauve8 data-[disabled]:pointer-events-none data-[highlighted]:bg-violet9 data-[highlighted]:text-violet1',
+  'group text-[13px] leading-none dark:text-white font-semibold rounded-[3px] flex items-center h-[25px] px-[5px] relative pl-[25px] select-none outline-none data-[disabled]:text-mauve8 data-[disabled]:pointer-events-none data-[highlighted]:bg-violet9 data-[highlighted]:text-violet1',
 );

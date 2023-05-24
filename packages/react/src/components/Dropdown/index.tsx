219d472a6d5de69f28c8797fd6430453848bdcac
isabelamacedo <isabelacb.macedo@gmail.com>
2023-05-24 11:02:14 -0300

    feat: updated props

--- packages/react/src/components/Dropdown/index.tsx
+++ packages/react/src/components/Dropdown/index.tsx
@@ -2,10 +2,36 @@
 import React from 'react';
 import { DotsVerticalIcon } from '@radix-ui/react-icons';
 import * as DropdownMenu from '@radix-ui/react-dropdown-menu';
+
+import {
+  DropdownContentProps,
+  DropdownItemProps,
+  DropdownPortalProps,
+  DropdownRootProps,
+  DropdownTriggerProps,
+} from '../../models/dropdown';
+
 import { Button } from '../Button';
+
 import * as Styles from './styles';
 
-export const Dropdown = () => {
+export interface DropdownProps {
+  elementDropdown?: HTMLElement;
+  root?: DropdownRootProps;
+  trigger?: DropdownTriggerProps;
+  portal?: DropdownPortalProps;
+  content?: DropdownContentProps;
+  item?: DropdownItemProps;
+}
+
+export const Dropdown = ({
+  elementDropdown,
+  root,
+  trigger,
+  portal,
+  content,
+  item,
+}: DropdownProps) => {
   return (
     <DropdownMenu.Root>
       <DropdownMenu.Trigger>
@@ -16,7 +42,7 @@ export const Dropdown = () => {
         />
       </DropdownMenu.Trigger>
 
-      <DropdownMenu.Portal>
+      <DropdownMenu.Portal container={elementDropdown}>
         <DropdownMenu.Content
           className={Styles.dropdownContent()}
           sideOffset={5}

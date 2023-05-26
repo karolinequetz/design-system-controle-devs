fe3f0e14fbee731ce0c060a67bccd6320bbd3d65
Karoline de Souza Quetz <kquetz@viceri.com.br>
2023-05-26 11:42:59 -0300

    fix: tweaks to storybook select

--- packages/docs/src/stories/Select.stories.tsx
+++ packages/docs/src/stories/Select.stories.tsx
@@ -1,33 +1,28 @@
-import React, { ReactNode } from "react";
+import React from "react";
 import type { StoryObj, Meta } from "@storybook/react";
 import { Select, SelectProps } from "@controle-devs-ui/react";
 
 export default {
   title: "Components/Select",
   component: Select,
+  decorators: [
+    (Story) => (
+      <div className="absolute flex flex-col w-64 left-0 overflow-x-hidden">
+        <Story />
+      </div>
+    ),
+  ],
 } as Meta<SelectProps>;
 
-type DecoratorProps = {
-  children: ReactNode;
-};
-const Decorator: React.FC<DecoratorProps> = ({ children }) => (
-  <div className="absolute flex flex-col w-64 left-0 bg-red-600 overflow-x-hidden">
-    {children}
-  </div>
-);
-
-export const Primary: StoryObj<SelectProps> = (args) => (
-  <Decorator>
-    <Select {...args} />
-  </Decorator>
-);
-Primary.args = {
-  placeholder: "Selecione as hard skills ...",
-  descriptiveTextForAccessibility: "select com opções de hard skills ",
-  options: [
-    { id: "1", description: "React Js" },
-    { id: "2", description: "React Native" },
-    { id: "3", description: "Angular" },
-    { id: "4", description: "C#" },
-  ],
+export const Primary: StoryObj<SelectProps> = {
+  args: {
+    placeholder: "Selecione as hard skills ...",
+    descriptiveTextForAccessibility: "select com opções de hard skills ",
+    options: [
+      { id: "1", description: "React Js" },
+      { id: "2", description: "React Native" },
+      { id: "3", description: "Angular" },
+      { id: "4", description: "C#" },
+    ],
+  },
 };

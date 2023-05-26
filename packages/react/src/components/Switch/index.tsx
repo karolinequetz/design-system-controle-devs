2405223c139c5c6f5bdcf161905c0f2520fa5a64
isabelamacedo <isabelacb.macedo@gmail.com>
2023-05-26 09:46:43 -0300

    feat: switch

--- packages/react/src/components/Switch/index.tsx
+++ packages/react/src/components/Switch/index.tsx
@@ -1,21 +1,36 @@
 'use client';
 import React from 'react';
 import * as SwitchUI from '@radix-ui/react-switch';
+import { SwitchRootProps, SwitchThumbProps } from '../../models/switch';
 import * as Styles from './styles';
 
-interface Props {
+export interface SwitchProps {
   label: string;
+  root: SwitchRootProps;
+  thumb: SwitchThumbProps;
 }
 
-const Switch = ({ label }: Props) => (
+export const Switch = ({ label, root, thumb }: SwitchProps) => (
   <div className={Styles.container()}>
     <label className={Styles.label()} htmlFor="airplane-mode">
       {label}
     </label>
-    <SwitchUI.Root className={Styles.switchRoot()} id="airplane-mode">
-      <SwitchUI.Thumb className={Styles.switchThumb()} />
+    <SwitchUI.Root
+      className={Styles.switchRoot()}
+      asChild={root.asChild}
+      defaultChecked={root.defaultChecked}
+      checked={root.checked}
+      onCheckedChange={root.onCheckedChange}
+      disabled={root.disabled}
+      required={root.required}
+      name={root.name}
+      value={root.value}
+      id="airplane-mode"
+    >
+      <SwitchUI.Thumb
+        className={Styles.switchThumb()}
+        asChild={thumb.asChild}
+      />
     </SwitchUI.Root>
   </div>
 );
-
-export default Switch;

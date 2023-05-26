31a5406469bdc1fed5c2256a8c16f708bd2e9f18
Karoline de Souza Quetz <kquetz@viceri.com.br>
2023-05-26 09:15:19 -0300

    fix: removed comments

--- packages/docs/src/stories/Checkbox.stories.tsx
+++ packages/docs/src/stories/Checkbox.stories.tsx
@@ -10,13 +10,13 @@ export const Default: StoryObj<CheckboxProps> = {
   args: {
     text: 'checkbox controle de devs',
     root: {
-      /*      defaultChecked: false,
-      disabled: false, */
+      defaultChecked: false,
+      disabled: false,
       onCheckedChange: () => console.log('alterar checkbox'),
-      /*       checked: true,
+      checked: true,
       required: false,
-      name: "teste",
-      value: "teste1", */
+      name: 'teste',
+      value: 'teste1',
     },
     indicator: {
       asChild: false,

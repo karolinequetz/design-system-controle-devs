c69c42dc6978959f63e6fc5ac174348703e3cbfe
Karoline de Souza Quetz <kquetz@viceri.com.br>
2023-05-29 19:22:39 -0300

    fix: adjustments to the component

--- packages/react/src/components/MultiSelect/index.tsx
+++ packages/react/src/components/MultiSelect/index.tsx
@@ -1,12 +1,11 @@
-import React from "react";
-import Select, { MultiValue, GroupBase } from "react-select";
+import React, { useState } from "react";
+import Select, { MultiValue } from "react-select";
 import { Options } from "../../models/select";
-import makeAnimated from "react-select/animated";
 
 export interface MultiSelectProps {
-  options: (Options | GroupBase<Options>)[];
+  options: Options[];
   placeholder?: string;
-  setState: (value: (string | number | GroupBase<string>)[]) => void;
+  setState?: (value: (string | number)[]) => void;
   name?: string;
   isClearable?: boolean;
   isSearchable?: boolean;
@@ -19,31 +18,32 @@ export const MultiSelect = ({
   options,
   placeholder = "Selecione as opções",
   name,
-  setState,
   isClearable,
   isSearchable,
   isDisabled,
   isLoading,
   closeMenuOnSelect,
 }: MultiSelectProps) => {
-  const animatedComponents = makeAnimated();
-  const onChange = (option: MultiValue<Options | GroupBase<Options>>) => {
+  const [state, setState] = useState<string[]>([]);
+  const onChange = (option: MultiValue<Options>) => {
     setState((option as Options[]).map((item: Options) => item.value));
   };
 
   return (
-    <Select
-      options={options}
-      name={name}
-      onChange={() => onChange}
-      components={animatedComponents}
-      isMulti
-      placeholder={placeholder}
-      isClearable={isClearable}
-      isSearchable={isSearchable}
-      isDisabled={isDisabled}
-      isLoading={isLoading}
-      closeMenuOnSelect={closeMenuOnSelect}
-    />
+    <>
+      <Select
+        options={options}
+        name={name}
+        onChange={onChange}
+        isMulti
+        placeholder={placeholder}
+        isClearable={isClearable}
+        isSearchable={isSearchable}
+        isDisabled={isDisabled}
+        isLoading={isLoading}
+        closeMenuOnSelect={closeMenuOnSelect}
+      />
+      <div className="text-white">{JSON.stringify(state)}</div>
+    </>
   );
 };

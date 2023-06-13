2f089840857b3cfd2d5bb49a8b0a381b40788f11
isabelamacedo <isabelacb.macedo@gmail.com>
2023-06-13 14:45:15 -0300

    feat: search input

---
+++ packages/react/src/components/SearchInput/index.tsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import { FaSearch } from 'react-icons/fa';
+import { Input } from '../Input';
+import * as Styles from './styles';
+
+export interface SearchInputProps {
+  placeholder: string;
+  onChange: (value: string) => void;
+}
+
+export const SearchInput = ({ placeholder, onChange }: SearchInputProps) => {
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    setSearchQuery(value);
+    onChange(value);
+  };
+
+  return (
+    <div className={Styles.container()}>
+      <Input
+        placeholder={placeholder}
+        value={searchQuery}
+        onChange={handleChange}
+      />
+      <FaSearch className={Styles.icon()} />
+    </div>
+  );
+};

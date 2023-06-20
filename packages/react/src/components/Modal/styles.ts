b9db4b816d8db14136b2c65f5a3bbc5b239cfd1f
isabelamacedo <isabelacb.macedo@gmail.com>
2023-06-20 18:24:59 -0300

    fix: height

--- packages/react/src/components/Modal/styles.ts
+++ packages/react/src/components/Modal/styles.ts
@@ -1,11 +1,11 @@
 import { cva } from 'class-variance-authority';
 
 export const overlay = cva(
-  'bg-blackA9 data-[state=open]:animate-overlayShow fixed inset-0 ',
+  'bg-blackA9 data-[state=open]:animate-overlayShow fixed inset-0',
 );
 
 export const content = cva(
-  'data-[state=open]:animate-contentShow bg-white dark:bg-neutral-900 dark:text-white fixed top-[50%] left-[50%] max-h-[85vh] w-[90vw] max-w-[450px] translate-x-[-50%] translate-y-[-50%] rounded-[6px]  p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none',
+  'data-[state=open]:animate-contentShow bg-white dark:bg-neutral-900 dark:text-white fixed top-[50%] left-[50%] w-[90vw] max-w-[450px] translate-x-[-50%] translate-y-[-50%] rounded-[6px] p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none',
 );
 
 export const title = cva(

0c8870c284c84abe65838804bac5174c8dd1f69d
isabelamacedo <isabelacb.macedo@gmail.com>
2023-06-20 18:52:47 -0300

    fix: width

--- packages/react/src/components/Modal/styles.ts
+++ packages/react/src/components/Modal/styles.ts
@@ -5,7 +5,7 @@ export const overlay = cva(
 );
 
 export const content = cva(
-  'data-[state=open]:animate-contentShow bg-white dark:bg-neutral-900 dark:text-white fixed top-[50%] left-[50%] w-[90vw] max-w-[450px] translate-x-[-50%] translate-y-[-50%] rounded-[6px] p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none',
+  'data-[state=open]:animate-contentShow bg-white dark:bg-neutral-900 dark:text-white fixed top-[50%] left-[50%] w-auto max-w-[450px] translate-x-[-50%] translate-y-[-50%] rounded-[6px] p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none',
 );
 
 export const title = cva(
